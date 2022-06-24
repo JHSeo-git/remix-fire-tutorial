adcbdff1431ba50625885b9e93571a68451b1c27
JHSeo <qnfqnfqnf@gmail.com>
2022-06-24 21:45:35 +0900

    feat: add routes

--- apps/remix-jokes-app/app/routes/index.tsx
+++ apps/remix-jokes-app/app/routes/index.tsx
@@ -1,3 +1,20 @@
-export default function Index() {
-  return <div className="container">Hello World</div>;
+import { Link } from '@remix-run/react';
+
+export default function IndexRoute() {
+  return (
+    <div className="container mx-auto h-full flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
+        <h1>
+          Remix <span className="block">Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
--- apps/remix-jokes-app/app/routes/jokex.tsx
+++
@@ -1,3 +0,0 @@
-export default function JokexIndex() {
-  return <div>Hello World</div>;
-}
